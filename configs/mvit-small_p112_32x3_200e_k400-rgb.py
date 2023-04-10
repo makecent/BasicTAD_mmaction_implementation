cd8cae3fdf0be1fc10dfbece07f87305c01f5646
makecent <luchongkai96@gmail.com>
2023-04-10 22:27:28 +0800

    ffe. add tensorboard backend.

--- configs/mvit-small_p112_32x3_200e_k400-rgb.py
+++ configs/mvit-small_p112_32x3_200e_k400-rgb.py
@@ -180,7 +180,7 @@ env_cfg = dict(
 
 log_processor = dict(type='LogProcessor', window_size=20, by_epoch=True)
 
-vis_backends = [dict(type='LocalVisBackend')]
+vis_backends = [dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend')]
 visualizer = dict(type='ActionVisualizer', vis_backends=vis_backends)
 
 log_level = 'INFO'
